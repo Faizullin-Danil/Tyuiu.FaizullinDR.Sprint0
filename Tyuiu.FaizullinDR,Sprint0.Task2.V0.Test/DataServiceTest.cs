124756677feee82653007ebd68d6241d90086230
Tyuiu.FaizullinDR@outlook.com <toxen2005@mail.ru>
2023-09-13 19:59:29 +0500

    Task2

---
+++ Tyuiu.FaizullinDR,Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.FaizullinDR.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.FaizullinDR_Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            //область создания методов тестирования, методов из библиотеки
+            var name = "Данил";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем класс Assert и метод AreEqua1
+            Assert.AreEqual("Привет..., Данил", res);
+        }
+    }
+}
---
+++ Tyuiu.FaizullinDR,Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.FaizullinDR.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.FaizullinDR_Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вызов класса DataService и метода GetMessage
+            //из библиотеки Tyuiu.FaizullinDRSprint0.Task2.V0.lib
+            Console.WriteLine(DataService.GetMessage("Данил"));
+            Console.ReadKey();
+            
+        }
+    }
+}
